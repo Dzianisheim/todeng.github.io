993405b5790ce0488479b7a3ec2f373b18adf86a
Dzianis Heiman <dzianis.heiman@itechart-group.com>
2019-05-24 16:27:04 +0300

    update

---
+++ precache-manifest.8eb10a730f6450177690c441055ba87a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ac4cd01cce19cf843432d1edaa2723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee7e697e760bcd595d15",
+    "url": "/static/css/main.d9a7c257.chunk.css"
+  },
+  {
+    "revision": "1e516b61cc9856f5527d",
+    "url": "/static/js/2.f6fdca74.chunk.js"
+  },
+  {
+    "revision": "ee7e697e760bcd595d15",
+    "url": "/static/js/main.3c9493af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
